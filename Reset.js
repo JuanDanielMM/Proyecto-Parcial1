60bbc9cb6fda211ff5f5c4e8012339f4e7d3278c
Nekomercat <l.a.b.tssj4@gmail.com>
2021-09-12 23:30:15 -0500

    Funcion para resetear campos de venta, precios y carrito

---
+++ Reset.js
@@ -0,0 +1,18 @@
+var idProdOrden = new Array();
+var cantOrden = new Array();
+var preciosOrden = new Array();
+const carrito = document.getElementById('cart');
+const total = document.getElementById('costoVenta');
+const catidad = document.getElementById('cantidad');
+
+function resetAll(){
+    idProdOrden = [];
+    cantOrden = [];
+    preciosOrden = [];
+    total.innerText = 0;
+    catidad.value = 0;
+    while (carrito.rows.length > 1) {
+        carrito.deleteRow(1);
+    }
+    
+}
